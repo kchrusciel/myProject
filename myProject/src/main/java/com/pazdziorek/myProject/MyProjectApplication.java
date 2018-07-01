8afe423898777a2b539b1d975b7e161cfbe4ad22
przemyslaw.pazdziorek <p.pazdziorek@gmail.com>
2018-07-01 22:24:20 +0200

    Finally workingn environment

--- myProject/src/main/java/com/pazdziorek/myProject/MyProjectApplication.java
+++ myProject/src/main/java/com/pazdziorek/myProject/MyProjectApplication.java
@@ -2,21 +2,29 @@ package com.pazdziorek.myProject;
 
 import com.pazdziorek.myProject.dataModel.FinancialData;
 import com.pazdziorek.myProject.domain.dataModel.DataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class MyProjectApplication {
+@ComponentScan({"com.pazdziorek"})
+public class MyProjectApplication implements CommandLineRunner {
 
+	@Autowired
     DataRepository dataRepository;
+    @Autowired
     FinancialData financialData;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyProjectApplication.class, args);
 	}
 
-	public void run (){
-        financialData = new FinancialData(1L,"Basic");
+    @Override
+    public void run(String... args) throws Exception {
+        Long id = 1L;
+        financialData = new FinancialData(id, "Basic");
         dataRepository.save(financialData);
-	}
+    }
 }
--- myProject/src/main/java/com/pazdziorek/myProject/domain/dataModel/DataRepository.java
+++ myProject/src/main/java/com/pazdziorek/myProject/domain/dataModel/DataRepository.java
@@ -2,6 +2,8 @@ package com.pazdziorek.myProject.domain.dataModel;
 
 import com.pazdziorek.myProject.dataModel.FinancialData;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DataRepository extends CrudRepository<FinancialData,Long>{
 }
